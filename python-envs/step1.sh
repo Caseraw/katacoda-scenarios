346ff2f675fd0c41342805c9f4dc1f8b6c624c72
Caseraw <kasra.amirsarvari@gmail.com>
2021-02-23 08:59:03 +0100

    Drafting

--- python-envs/step1.sh
+++ python-envs/step1.sh
@@ -1,9 +1,11 @@
+echo "Preparing the working area, please be patient."
+
 apt-get install -y ruby figlet &> /dev/null &&\
 mkdir ~/lolcat &&\
 wget -P ~/lolcat https://github.com/busyloop/lolcat/archive/master.zip &> /dev/null &&\
 unzip ~/lolcat/master.zip -d ~/lolcat &> /dev/null &&\
 cd ~/lolcat/lolcat-master/bin &> /dev/null &&\
-gem install lolcat &&\
+gem install lolcat &> /dev/null &&\
 cd ~ &&\
 clear &&\
 figlet -t -k "pyenv & venv" | lolcat &&\
