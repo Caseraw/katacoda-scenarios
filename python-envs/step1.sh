fecdb15a61056ddb77b8bcd641120c777adf38cf
Caseraw <kasra.amirsarvari@gmail.com>
2021-02-23 08:43:18 +0100

    Drafting

--- python-envs/step1.sh
+++ python-envs/step1.sh
@@ -4,4 +4,4 @@ wget -P ~/lolcat https://github.com/busyloop/lolcat/archive/master.zip &&\
 unzip ~/lolcat/master.zip &&\
 gem install ~/lolcat-master/bin/lolcat &&\
 clear &&\
-figlet -t -k pyenv | lolcat &&\
+figlet -t -k pyenv | lolcat
