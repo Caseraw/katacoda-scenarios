db0ca9059fdbc53d1a16df50c3ce46723aea4c2e
Caseraw <kasra.amirsarvari@gmail.com>
2021-03-09 17:24:53 +0100

    Drafting

--- certificate-factory/playbatch01.sh
+++ certificate-factory/playbatch01.sh
@@ -9,7 +9,10 @@ openssl genrsa -aes256 -passout pass:secret -out intermediate/private/site2.exam
 openssl genrsa -aes256 -passout pass:secret -out intermediate/private/site3.example.com.key.pem 2048
 openssl genrsa -aes256 -passout pass:secret -out intermediate/private/site4.example.com.key.pem 2048
 
-chmod 400 intermediate/private/intermediate/private/site*.example.com.key.pem
+chmod 400 intermediate/private/intermediate/private/site1.example.com.key.pem
+chmod 400 intermediate/private/intermediate/private/site2.example.com.key.pem
+chmod 400 intermediate/private/intermediate/private/site3.example.com.key.pem
+chmod 400 intermediate/private/intermediate/private/site4.example.com.key.pem
 
 openssl req -config intermediate/openssl.cnf -new -sha256 \
 -key intermediate/private/site1.example.com.key.pem \
@@ -59,7 +62,20 @@ openssl ca -batch -config intermediate/openssl.cnf -extensions server_cert \
 -in intermediate/csr/site4.example.com.csr.pem \
 -out intermediate/certs/site4.example.com.cert.pem
 
+clear
+
+tree /root/ca/intermediate/certs/
+
+tree /root/ca/intermediate/private/
+
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/site1.example.com.cert.pem
+openssl x509 -noout -in intermediate/certs/site1.example.com.cert.pem -issuer -subject -dates
+
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/site2.example.com.cert.pem
+openssl x509 -noout -in intermediate/certs/site2.example.com.cert.pem -issuer -subject -dates
+
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/site3.example.com.cert.pem
+openssl x509 -noout -in intermediate/certs/site3.example.com.cert.pem -issuer -subject -dates
+
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/site4.example.com.cert.pem
+openssl x509 -noout -in intermediate/certs/site4.example.com.cert.pem -issuer -subject -dates
--- certificate-factory/step5.md
+++ certificate-factory/step5.md
@@ -1,1 +1,7 @@
-...
+This step provides 4 server certificates:
+
+- site1.example.com
+- site2.example.com
+- site3.example.com
+- site4.example.com
+
