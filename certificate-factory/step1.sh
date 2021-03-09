97ab34877588d8256d11f5bd9137af4ac2c74afa
Caseraw <kasra.amirsarvari@gmail.com>
2021-03-09 12:41:11 +0100

    Drafting

--- certificate-factory/step1.sh
+++ certificate-factory/step1.sh
@@ -4,4 +4,4 @@ mkdir certs crl newcerts private &&\
 chmod 700 private &&\
 touch index.txt &&\
 echo 1000 > serial &&\
-wget 
+wget -O openssl.cnf https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ca_openssl.cnf
