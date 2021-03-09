66da44bc42e378dfdf65a325f65ed58478d8996d
Caseraw <kasra.amirsarvari@gmail.com>
2021-03-09 19:37:20 +0100

    Drafting

--- certificate-factory/ansible_solution01.sh
+++ certificate-factory/ansible_solution01.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p /root/ca/ansible_solution01
 mkdir -p /root/ca/ansible_solution01/collections/ansible_collections
 cd /root/ca/ansible_solution01
 
-wget playbook.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_playbook.yml"
-wget hosts "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_hosts"
-wget collections/requirements.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_collections_requirements.yml"
+wget -O playbook.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_playbook.yml"
+wget -O hosts "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_hosts"
+wget -O collections/requirements.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_collections_requirements.yml"
 
-ansible-galaxy collection install -r collections/ansible_collections/requirements.yml
+cd /root/ca/ansible_solution01
+ansible-galaxy collection install -r collections/requirements.yml
