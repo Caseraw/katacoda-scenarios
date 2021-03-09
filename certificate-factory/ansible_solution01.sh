a5f394d971d9f18fb1821515f1067e598c1c533e
Caseraw <kasra.amirsarvari@gmail.com>
2021-03-09 19:32:19 +0100

    Drafting

--- certificate-factory/ansible_solution01.sh
+++ certificate-factory/ansible_solution01.sh
@@ -6,6 +6,6 @@ cd /root/ca/ansible_solution01
 
 wget ansible_solution01_playbook.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_playbook.yml"
 wget ansible_solution01_hosts "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_hosts"
-wget collections/ansible_collections/ansible_solution01_collections_requirements.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_collections_requirements.yml"
+wget collections/ansible_solution01_collections_requirements.yml "https://raw.githubusercontent.com/Caseraw/katacoda-scenarios/main/certificate-factory/ansible_solution01_collections_requirements.yml"
 
-ansible-galaxy collection install -r requirements.yml
+ansible-galaxy collection install -r collections/ansible_collections/requirements.yml
